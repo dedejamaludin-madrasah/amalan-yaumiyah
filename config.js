51f0a994f84f5d76e3320cd313d6b4ec2c2a78f5
dedejamaludin-madrasah <dedejamaludin@madrasah.kemenag.go.id>
2025-12-26 03:05:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // GANTI DENGAN KUNCI SUPABASE ANDA
-const SUPABASE_URL = 'https://URL_PROJECT_ANDA.supabase.co';
-const SUPABASE_KEY = 'KEY_ANON_PANJANG_ANDA';
+const SUPABASE_URL = 'https://iinprkhxulmpioqakdte.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpbnBya2h4dWxtcGlvcWFrZHRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NTkyNzAsImV4cCI6MjA4MjIzNTI3MH0.nVXeKEE5kiWV_Uvmd-H-5pTk48MLm981Njdmujtsq28';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -24,4 +24,5 @@ function logout() {
 function getIndoDate() {
     const d = new Date();
     return d.toLocaleDateString('id-ID', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
+
 }
